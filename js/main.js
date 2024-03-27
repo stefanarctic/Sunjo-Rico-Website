70eb56df460025e982f128200ba867e51b307324
stefanarctic <87850525+stefanarctic@users.noreply.github.com>
2024-03-27 23:25:42 +0200

    Header not showing in hosted version fixed

--- js/main.js
+++ js/main.js
@@ -8,4 +8,14 @@ window.onload = () => {
     videoElements.forEach(video => {
         video.volume = 0;
     });
+
+    // Because relative path doesn't work in the hosted version
+    const mainDiv = document.getElementById('main');
+    mainDiv.style.setProperty('background', `url('${document.location.origin}/images/Night_City1.png') no-repeat center fixed`);
+    mainDiv.style.setProperty('background-size', 'cover');
+    // console.clear();
+    // console.log("asdsa");
+    // console.log(mainDiv);
+    // console.log(document.location.origin);
+    // mainDiv.setAttribute('background', `url('${document.location.origin}/images/Night_City1.png') no-repeat center fixed`);
 }
